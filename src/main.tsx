1d9b3662a80f1772896e9e7c205d64c31e6a6a0d
hadsome-Droid <xkumbhax@gmail.com>
2025-02-16 18:31:39 +0300

    refactor import main

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { Provider } from 'react-redux'
 
-import { store } from '@/app/store'
 import ReactDOM from 'react-dom/client'
 
 import './styles/index.scss'
 
 import App from './App'
+import { store } from './app/store'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
