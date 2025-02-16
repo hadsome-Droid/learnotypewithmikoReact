7e6a50aebf8515eac033aa82e4245544f0e3f5cd
hadsome-Droid <xkumbhax@gmail.com>
2025-02-16 19:12:10 +0300

    refactor vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import * as path from 'path';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 export default defineConfig({
+    base: 'https://hadsome-droid.github.io/learnotypewithmikoReact/',
+    build: {
+        outDir: 'dist',
+    },
     plugins: [react()],
     resolve: {
         alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
